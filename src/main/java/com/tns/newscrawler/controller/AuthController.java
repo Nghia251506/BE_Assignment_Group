0db26a150e26fa6516308fcb0309155b6ab2c52c
TNC <89584074+Nghia251506@users.noreply.github.com>
2025-11-27 13:24:09 +0700

    Update AuthController.java

--- src/main/java/com/tns/newscrawler/controller/AuthController.java
+++ src/main/java/com/tns/newscrawler/controller/AuthController.java
@@ -64,7 +64,6 @@ public class AuthController {
             cookie.setSecure(true); // Chỉ gửi cookie qua HTTPS
             cookie.setPath("/"); // Cookie có hiệu lực trên toàn bộ ứng dụng
             cookie.setMaxAge(7 * 24 * 60 * 60); // Cookie tồn tại trong 7 ngày
-            cookie.setDomain(".muong14.xyz");
             response.addCookie(cookie);
 
             // Lấy thông tin UserDto từ service
