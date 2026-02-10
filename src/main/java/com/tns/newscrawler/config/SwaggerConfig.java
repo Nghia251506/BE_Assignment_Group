a056f2467a5e38eefd8f360cf933275a7210d841
TNC <89584074+Nghia251506@users.noreply.github.com>
2026-02-10 07:42:06 +0700

    Update SwaggerConfig.java

--- src/main/java/com/tns/newscrawler/config/SwaggerConfig.java
+++ src/main/java/com/tns/newscrawler/config/SwaggerConfig.java
@@ -1,61 +1,34 @@
 package com.tns.newscrawler.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 
-import java.util.Collections;
-@OpenAPIDefinition(
-        info = @Info(
-                title = "My API",
-                version = "v1",
-                description = "API documentation for my Spring Boot application"
-        )
-)
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.tns.newscrawler.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .securitySchemes(Collections.singletonList(apiKey())) // Đảm bảo rằng API Key đã được cấu hình
-                .securityContexts(Collections.singletonList(securityContext()));
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("News Crawler API")
-                .description("API Documentation for the News Crawler Application")
-                .version("1.0")
-                .build();
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header"); // Cấu hình Authorization header với JWT
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(Collections.singletonList(
-                        new SecurityReference("JWT", new AuthorizationScope[]{
-                                new AuthorizationScope("global", "accessEverything")
-                        })))
-                .build();
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        
+        return new OpenAPI()
+                .info(new Info()
+                        .title("News Crawler API")
+                        .version("1.0")
+                        .description("API Documentation for the News Crawler Application"))
+                // Thêm cấu hình Security để có nút "Authorize" trên giao diện Swagger
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
